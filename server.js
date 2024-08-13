36c5bddfcee2b20424d1e7babf302a9cfe1a205d
Jonny0430 <ahadjon697gmail.com>
2024-08-14 07:32:16 +0900

    fear: Reja Modify & harid.ejs logic

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require('http');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 //1: kirish code
 app.use(express.static("public"));
@@ -16,15 +26,21 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 routing code
-app.get("/hello", function(req, res){
-    res.end("<h1>Hello World Jonny</h1>");
+app.post("/create-item", (req, res) => {
+//   console.log(req.body);
+//   res.json({test: "success"});
 });
 
-app.get("/gift", function(req, res){
-    res.end("<h1>Siz sovgalar bolimidasiz</h1>");
+app.get("/auhtor", (req, res) => {
+  res.render("author", {user: user});
+});
+
+app.get("/", function(req, res) {
+    res.render("harid");
 });
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
-    console.log(`The server is running successfully on port:${PORT}`);
+        console.log(`The server is running successfully on port:${PORT}`);
+        console.log(`The server is running successfully on http://localhost:${PORT}`);
 });
