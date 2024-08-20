b25829f0c17538f2cc9d736b583a49c0a6a7ca7f
Jonny0430 <ahadjon697gmail.com>
2024-08-20 11:30:44 +0900

    fix: Asynchronous funcionlarni qo'llash tuzatish kiritildi

--- train.js
+++ train.js
@@ -12,14 +12,14 @@ const list = [
     "endi dam oling, foydasi yoq endi", //60
 ];
 function maslahatBering(a, callback) {
-    if (typeof a !== "number") callback("insert a number", null);
+    if (typeof a !== "number") callback("Insert a number", null);
     else if (a <= 20) callback(null, list[0]);
     else if (a > 20 && a <= 30) callback(null, list[1]);
     else if (a > 30 && a <= 40) callback(null, list[2]);
     else if (a > 40 && a <= 50) callback(null, list[3]);
     else if (a > 50 && a <= 60) callback(null, list[4]);
     else {
-        setTimeout(function () {
+        setInterval(function () { //setTimeout
         callback(null, list[5]);
       }, 1000);
     }
