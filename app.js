418f41128331ddd99f5e3fb781a5aea7401209ab
Jonny0430 <ahadjon697gmail.com>
2024-08-24 16:49:06 +0900

    fix:Reja mini loyihamizning frontendini quramiz

--- app.js
+++ app.js
@@ -35,26 +35,28 @@ app.post("/create-item", (req, res) => {
   // res.json({test: "success" });
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if(err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    // if(err) {
+    //   console.log(err);
+    //   res.end("something went wrong");
+    // } else {
+    //   res.end("successfully added");
+    // }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
 
 
-app.get("/author", (req, res) => {
-  res.render("author", {user: user});
-});
+// app.get("/author", (req, res) => {
+//   res.render("author", {user: user});
+// });
 
 app.get("/", function(req, res) {
-  console.log('user entered /');
+  console.log("user entered /");
   db.collection("plans")
   .find()
-  .toArray((err,data) => {
+  .toArray((err, data) => {
     if (err) {
       console.log(err);
       res.end("something went wrong");
